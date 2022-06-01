f6f031379d903913575f2475946dae6238a03f66
xjkxksj <106696465+xjkxksj@users.noreply.github.com>
2022-06-01 20:49:39 +0200

    Add files via upload

---
+++ Projekt1/Sedzia.cs
@@ -0,0 +1,15 @@
+﻿class Sedzia
+{
+    protected string imie, nazwisko;
+    public Sedzia(string imie, string nazwisko)
+    {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+    }
+}
+class Sedzia_pomocniczy : Sedzia
+{
+    public Sedzia_pomocniczy(string imie, string nazwisko) : base(imie, nazwisko)
+    {
+    }
+}
