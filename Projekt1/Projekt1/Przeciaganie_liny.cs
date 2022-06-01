e57304d4700c88351ddba8ab6cd430d090607bab
xjkxksj <106696465+xjkxksj@users.noreply.github.com>
2022-06-01 20:48:42 +0200

    Delete Projekt1/Projekt1 directory

--- Projekt1/Projekt1/Przeciaganie_liny.cs
+++
@@ -1,7 +0,0 @@
-﻿class Przeciaganie_liny : Gra
-    {
-        public Przeciaganie_liny(Druzyna pierwsza_druzyna, Druzyna druga_druzyna, Sedzia pierwszy_sedzia) : base(pierwsza_druzyna, druga_druzyna, pierwszy_sedzia)
-        {
-        }
-    }
-
