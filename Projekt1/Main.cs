64b4d4da44c74fcf390e2ee7f942296aa52f362f
jradlen87 <106774650+jradlen87@users.noreply.github.com>
2022-06-07 12:22:56 +0200

    Update Main.cs

--- Projekt1/Main.cs
+++ Projekt1/Main.cs
@@ -1,4 +1,4 @@
-﻿using Projekt1;
+using Projekt1;
 using System.Collections.Generic;
 
 class Projekt
@@ -39,16 +39,34 @@ class Projekt
                     System.Console.WriteLine("[D] - Dodaj");
                     System.Console.WriteLine("[U] - Usun");
                     System.Console.WriteLine("[P] - Przejrzyj");
+                    System.Console.WriteLine("[S] - Tabela druzyn w siatkowce");
+                    System.Console.WriteLine("[O] - Tabela druzyn w dwoch ogniach");
+                    System.Console.WriteLine("[L] - Tabela druzyn w przeciaganiu liny");
                     switch (System.Console.ReadKey().Key)
                     {
                         case System.ConsoleKey.D:
+                            System.Console.Clear();
                             zawody.Dodaj_Druzyne();
                             break;
                         case System.ConsoleKey.U:
+                            System.Console.Clear();
                             zawody.Usun_Druzyne();
                             break;
                         case System.ConsoleKey.P:
+                            System.Console.Clear();
                             zawody.Przeglad_Druzyny_String();
+                            break;
+                        case System.ConsoleKey.S:
+                            System.Console.Clear();
+                            zawody.Tabela_Siatkowka_String();
+                            break;
+                        case System.ConsoleKey.O:
+                            System.Console.Clear();
+                            zawody.Tabela_Dwaognie_String();
+                            break;
+                        case System.ConsoleKey.L:
+                            System.Console.Clear();
+                            zawody.Tabela_Przeciaganieliny_String();
                             break;
                     }
                     break;
@@ -56,16 +74,41 @@ class Projekt
                     System.Console.WriteLine("[S] - Mecz w siatkowke");
                     System.Console.WriteLine("[D] - Mecz w dwa ognie");
                     System.Console.WriteLine("[P] - Mecz w przeciaganie liny");
+                    System.Console.WriteLine("[F] - Finaly");
                     switch (System.Console.ReadKey().Key)
                     {
                         case System.ConsoleKey.S:
+                            System.Console.Clear();
                             zawody.Rozgrywka_Siatkowka();
                             break;
                         case System.ConsoleKey.D:
+                            System.Console.Clear();
                             zawody.Rozgrywka_Dwa_Ognie();
                             break;
                         case System.ConsoleKey.P:
+                            System.Console.Clear();
                             zawody.Rozgrywka_Przeciaganie_Liny();
+                            break;
+                        case System.ConsoleKey.F:
+                            System.Console.Clear();
+                            System.Console.WriteLine("[S] - Final siatkowki");
+                            System.Console.WriteLine("[D] - Final dwoch ogni");
+                            System.Console.WriteLine("[P] - Final przeciagania liny");
+                            switch(System.Console.ReadKey().Key)
+                            {
+                                case System.ConsoleKey.S:
+                                    System.Console.Clear();
+                                    zawody.Finaly("Siatkowka");
+                                    break;
+                                case System.ConsoleKey.D:
+                                    System.Console.Clear();
+                                    zawody.Finaly("Dwa Ognie");
+                                    break;
+                                case System.ConsoleKey.P:
+                                    System.Console.Clear();
+                                    zawody.Finaly("Przeciaganie Liny");
+                                    break;
+                            }
                             break;
                     }
                     break;
